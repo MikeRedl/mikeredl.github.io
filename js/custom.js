ad65e880299a221e0cc8f462aa0ca5ea19b49e3e
Michael Redl <Michael.Redl@web.de>
2022-12-07 14:31:08 +0100

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function() {
+    
+    
+   
+    
+
+     $('#exprt').click(function(){
+          document.getElementById('expertiseSection').scrollIntoView({block: 'start'});
+     }); 
+     $('#expr').click(function(){
+          document.getElementById('experienceSection').scrollIntoView({block: 'center'});
+     });  
+     $('#qual').click(function(){
+          document.getElementById('qualificationSection').scrollIntoView({block: 'center'});
+     });  
+      
+    
+    });
