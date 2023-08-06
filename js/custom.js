da90c9eae5c51485e5bdde4fc05532e7d3c8eac3
Michael Redl <Michael.Redl@web.de>
2023-08-06 11:22:57 +0200

    added mobile navigation

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,33 @@
 
 
-$(document).ready(function() {
-    
-    
-   
-    
+$(document).ready(function () {
 
-     $('#exprt').click(function(){
-          document.getElementById('expertiseSection').scrollIntoView({block: 'start'});
-     }); 
-     $('#expr').click(function(){
-          document.getElementById('experienceSection').scrollIntoView({block: 'start'});
-     });  
-     $('#qual').click(function(){
-          document.getElementById('qualificationSection').scrollIntoView({block: 'start'});
-     });  
-      
-    
-    });
+     let isClicked = false;
+
+
+
+
+     $('#exprt').click(function () {
+          document.getElementById('expertiseSection').scrollIntoView({ block: 'start' });
+     });
+     $('#expr').click(function () {
+          document.getElementById('experienceSection').scrollIntoView({ block: 'start' });
+     });
+     $('#qual').click(function () {
+          document.getElementById('qualificationSection').scrollIntoView({ block: 'start' });
+     });
+
+     $('.HamburgerIcon').click(() => {
+          if (isClicked) {
+               $('.mobileNav').css("display", "none");
+               isClicked = false;
+          }
+          else {
+               $('.mobileNav').css("display", "block");
+               isClicked = true;
+          }
+     });
+
+
+
+});
