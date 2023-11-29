cff7de57dbe4f9d13bf95fb09569178359f7bff7
Michael  Redl <m.redl@michael-redl.com>
2023-11-29 15:42:36 +0100

    added visit counter

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,38 @@
 
 
 $(document).ready(function () {
+     // Define the URL of your Azure Function
+     const functionUrl = 'https://websiteform.azurewebsites.net/api/HttpTrigger1';
 
+     // Define the data you want to send
+     const requestData = {
+          add: 1
+     };
 
-
-     $('#apiButton').click(function () {
-          // Define the URL of your Azure Function
-          const functionUrl = 'https://websiteform.azurewebsites.net/api/HttpTrigger1';
-
-          // Define the data you want to send
-          const requestData = {
-               name: 'TestName'
-          };
-
-          // Make the HTTP request
-          fetch(functionUrl, {
-               method: 'POST', // Assuming your function is expecting a POST request
-               headers: {
-                    'Content-Type': 'application/json'
-                    // If your function requires an API key or authentication, add it here
-               },
-               body: JSON.stringify(requestData) // Convert the JavaScript object to a JSON string
+     // Make the HTTP request
+     fetch(functionUrl, {
+          method: 'POST', // Assuming your function is expecting a POST request
+          headers: {
+               'Content-Type': 'application/json'
+               // If your function requires an API key or authentication, add it here
+          },
+          body: JSON.stringify(requestData) // Convert the JavaScript object to a JSON string
+     })
+          .then(response => {
+               if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+               }
+               return response.json(); // or `response.text()` if the response is not in JSON format
           })
-               .then(response => {
-                    if (!response.ok) {
-                         throw new Error('Network response was not ok ' + response.statusText);
-                    }
-                    return response.json(); // or `response.text()` if the response is not in JSON format
-               })
-               .then(data => {
-                    console.log('Success:', data); // Handle the response data
-               })
-               .catch((error) => {
-                    console.error('Error:', error); // Handle any errors
-               });
+          .then(data => {
+               console.log('Success:', data); // Handle the response data
+          })
+          .catch((error) => {
+               console.error('Error:', error); // Handle any errors
+          });
 
-     });
 
+   
 
 
 
